4648bea438ab666e4361c8f2e26f2bffb6c798ce
ggyu <kcpoooh0202@naver.com>
2020-11-03 09:54:30 +0900

    factorial

--- log.js
+++ log.js
@@ -1,9 +1,19 @@
-function factorial(n){
-    if(n === 1){
-        return 1;
-    }
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
 
-    return n * factorial(n - 1);
+  return n * factorial(n - 1);
 }
 
 console.log(BigInt(factorial(100)));
+
+function factorial2(n) {
+  let i,
+    result = BigInt(1);
+  for (i = BigInt(2); i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+console.log(factorial2(100));
